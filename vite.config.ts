556ec8038c6d123daf4a594f089fdd20c86c4b2a
Bartolo de la Rosa Rivera <157636952+Bdelarosarivera@users.noreply.github.com>
2026-04-29 13:47:26 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: './', 
@@ -14,5 +13,5 @@ export default defineConfig({
         }
       }
     }
-  },
+  }
 });
